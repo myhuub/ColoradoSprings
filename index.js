92d0469064c84c99c2aacd0ff754c2e43c4002b2
myhuub <93978100+myhuub@users.noreply.github.com>
2022-10-18 13:02:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import {View, Text,TextInput} from "react-native";
 import { WebView } from 'react-native-webview';
 import App from './App'
 export const applyCustomCode = externalCodeSetup => {
-        applyCustomCode.navigationOptions = {
-        header: null,
-        title:"Sign Up",
-        headerTitle:"Sign Up"
-          }
-         externalCodeSetup.navigationApi.setParams({ title: "Sign Up" })
+  //      applyCustomCode.navigationOptions = {
+  //      header: null,
+ //       title:"Sign Up",
+ //       headerTitle:"Sign Up"
+ //         }
+  //       externalCodeSetup.navigationApi.setParams({ title: "Sign Up" })
     // call custom code api here
-     externalCodeSetup.configApi.setAppSwitchEnabled(true);
+    //externalCodeSetup.configApi.setAppSwitchEnabled(true);
     externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen",App);
 };
-
